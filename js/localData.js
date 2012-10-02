b845c9b3a34c094cc1683862a78293b1d72e0ce6
abrudtkuhl <abrudtkuhl@gmail.com>
2012-10-02 16:45:33 -0500

    keep track of what step you are on using localStorage

--- js/localData.js
+++ js/localData.js
@@ -1,24 +1,36 @@
 (function($) {
 	$.localData = {
-		saveAll: function storeData() {
+		saveAllInputs: function storeData() {
 			$('input').each(function() {
 				console.log("storing data -> " + $(this).attr("name") + ":" + $(this).val());
 				$.jStorage.set($(this).attr("name"), $(this).val());
 			});
 		},
-		saveItem: function storeDataItem(object) {
+		saveObject: function storeDataItem(object) {
 			console.log("saving item data -> " + $(object).val());
 			$.jStorage.set($(object).attr("name"), $(object).val());
 		},
-		get: function getData() {
+		saveItem: function storeDataItem(key, value) {
+			$.jStorage.set(key,value)
+			console.log("saving data item -> " + key + " = " + value);
+		},
+		getAllInputs: function getData() {
 			console.log("getting data");
 			$('input').each(function() {
 				$(this).val($.jStorage.get($(this).attr("name")));
 			});
 		},
+		getItem: function getItem(key) {
+			return $.jStorage.get(key);
+		},
 		flush: function flushData() {
-			console.log("flusing local data");
+			console.log("flushing local data");
 			$.jStorage.flush();
-		}
+		},
+		getJSON: function getJSON() {
+			console.log("getting json data from local data storage");
+			ko.toJSON(this.viewModel);
+		},
+		viewModel: this.viewModel
 	}
 })(jQuery);
--- js/steps.js
+++ js/steps.js
@@ -1,17 +1,21 @@
 (function($) {
+	
 	$("#step1 .next").click(function() {
 		$("#step1, #step2").toggle();
 		updateProgress("20", "2");
+		$.localData.saveItem("step", "2");
 	});
 
 	$("#step2 .back").click(function() {
 		$("#step1, #step2").toggle();
+		$.localData.saveItem("step", "1");
 		updateProgress("10", "1");
 	});
 
 	$("#step2 .next").click(function() {
 		$("#step2, #step3").toggle();
 		updateProgress("100", "3");
+		$.localData.saveItem("step", "3");
 	});
 
 })(jQuery);
@@ -19,4 +23,15 @@
 function updateProgress(percent, step) {
 	$("#progress-step").html(step);
 	$(".progress .bar").css("width", percent + "%");
+}
+
+//* Show current step *//
+function checkStep() {
+	var step = $.jStorage.get("step", "1");
+
+	$(".step").each(function() {
+		$(this).hide();
+	});
+
+	$("#step" + step).show();
 }
