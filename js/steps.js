26c43ce3edf67036178299a46bc9db6eee3eaacc
Andy Brudtkuhl <abrudtkuhl@gmail.com>
2012-10-02 23:28:31 -0500

    integrated twitter bootstrap tabs for each seaction of the form. removed dynamic progress bar for now

--- js/steps.js
+++ js/steps.js
@@ -1,59 +1,32 @@
 (function($) {
 	
 	$("#step1 .next").click(function() {
-
 		if (!$("#formStep1").valid()) { 
 			return;
 		}
 
-		$("#formStep1").validate();
-		$("#step1, #step2").toggle();
-		updateProgress(2);
-		$.localData.saveItem("step", "2");
+		setTab("work");
 	});
 
 	$("#step2 .back").click(function() {
-		if (!$("#formStep2").valid()) { 
-			return;
-		}
-
-		$("#step1, #step2").toggle();
-		$.localData.saveItem("step", "1");
-		updateProgress(1);
+		setTab("contact");
 	});
 
 	$("#step2 .next").click(function() {
-		$("#step2, #step3").toggle();
-		updateProgress(3);
-		$.localData.saveItem("step", "3");
+		if (!$("#formStep2").valid()) { 
+			return;
+		}
+		setTab("review");
 	});
 
 })(jQuery);
 
-function updateProgress(step) {
-	$("#progress-step").html(step);
-
-	// this is pretty nasty... REFACTOR ME PLEASE!!!!!!!!!!!!
-	var percent = 0;
-
-	if (step == 1)
-		percent = 20;
-	if (step == 2)
-		percent = 60;
-	if (step == 3)
-		percent = 90;
-
-	$(".progress .bar").css("width", percent + "%");
+function setTab(tab) {
+	$('#tabs a[href="#' + tab + '"]').tab('show');
+	$.localData.saveItem("tab", tab);
 }
 
 //* Show current step *//
 function checkStep() {
-	$(".step").each(function() {
-		$(this).hide();
-	});
-
-	var step = $.jStorage.get("step", "1");
-
-	$("#step" + step).show();
-	updateProgress(step);
+	var step = $.jStorage.get("tab", "contact");
 }
