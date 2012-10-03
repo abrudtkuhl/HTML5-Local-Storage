59449175e9c7b9804ecad3cd30a30398db9dacca
abrudtkuhl <abrudtkuhl@gmail.com>
2012-10-02 19:23:55 -0500

    now using jquery validation plugin. also plays nice with validation messages from twitter bootstrap

--- js/steps.js
+++ js/steps.js
@@ -1,15 +1,25 @@
 (function($) {
 	
 	$("#step1 .next").click(function() {
+
+		if (!$("#formStep1").valid()) { 
+			return;
+		}
+
+		$("#formStep1").validate();
 		$("#step1, #step2").toggle();
-		updateProgress(1);
+		updateProgress(2);
 		$.localData.saveItem("step", "2");
 	});
 
 	$("#step2 .back").click(function() {
+		if (!$("#formStep2").valid()) { 
+			return;
+		}
+
 		$("#step1, #step2").toggle();
 		$.localData.saveItem("step", "1");
-		updateProgress(2);
+		updateProgress(1);
 	});
 
 	$("#step2 .next").click(function() {
