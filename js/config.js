dd3619ab34d0a25b9f6d85ef5521c6c6aef1d749
Andy Brudtkuhl <abrudtkuhl@gmail.com>
2012-10-16 12:27:22 -0500

    json configuration

---
+++ js/config.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	var configUrl = 'js/config.json';
+	$.getJSON(configUrl, function(data) {
+		customize(data);
+	});
+});
+
+function customize(config) {
+	console.log("customizing the UI based on local config");
+	console.log(config);
+
+	/* logo */
+	$("#logo").attr("src", config.logo);
+
+	/* tabs */
+	$(config.tabs).each(function(key,value) {
+		//alert(value.work);
+	});
+}
